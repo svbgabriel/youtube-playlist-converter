2a0a54a8146efced4ba8faa88cab187043609082
Gabriel Batista <svbgabriel@gmail.com>
2022-04-12 22:30:22 -0300

    feat: Add feature to change name and description of playlist

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 	"youtube-playlist-converter/config"
 )
 
 func main() {
 	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatal("Playlist ID not informed!")
+	}
 	id := args[0]
 
 	configuration := config.ReadConfig()
@@ -16,6 +22,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Looking for songs in playlist \"%s\"\n", playlist.Items[0].Snippet.Title)
 
 	titleList, err := readPlaylistItems(id, configuration.Youtube.Key)
 	if err != nil {
@@ -29,13 +37,32 @@ func main() {
 		log.Fatal(err)
 	}
 
+	fmt.Printf("Using the user %s\n", currentUser)
+
 	title := playlist.Items[0].Snippet.Title + " by " + playlist.Items[0].Snippet.ChannelTitle
 	description := playlist.Items[0].Snippet.Description
+
+	in := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("Enter a name for the playlist in Spotify [%s]:", title)
+	newTitle, _ := in.ReadString('\n')
+	newTitle = strings.TrimSpace(newTitle)
+	if len(newTitle) > 0 {
+		title = newTitle
+	}
+	fmt.Printf("Enter a description for the playlist [%s]:", description)
+	newDescription, _ := in.ReadString('\n')
+	newDescription = strings.TrimSpace(newDescription)
+	if len(newDescription) > 0 {
+		description = newDescription
+	}
 
 	playlistID, err := createPlaylist(currentUser, title, description, token)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("The playlist %s was created", title)
 
 	for _, item := range titleList {
 		tracks, _ := searchTrack(item, token)
